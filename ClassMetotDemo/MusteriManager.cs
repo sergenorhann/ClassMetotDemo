b91e803193c261af9890720a4ab189d52932321c
sergenorhann <sergenorhann@users.noreply.github.com>
2021-03-01 07:14:44 +0300

    03.01.2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        int MusteriSayisi = 0;
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi.ToUpper() + " " + musteri.Soyadi.ToUpper() + " Başarıyla eklendi.\n");
+
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id.ToString() + " Id Numaralı " + musteri.Adi.ToUpper() +" "+ musteri.Soyadi.ToUpper() + " başarıyla silindi.\n");
+        }
+
+        public void MusteriListele(params Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.TcNo);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.Il);
+                Console.Write("\n");
+                MusteriSayisi++;
+            }
+            Console.WriteLine("Güncel Müşteri Sayısı : " + MusteriSayisi.ToString()+"\n");
+        }
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,29 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Id = 0;
+            musteri1.TcNo = 12345678901;
+            musteri1.Adi = "ali";
+            musteri1.Soyadi = "yılmaz";
+            musteri1.Il = "Adana";
+            musteriManager.MusteriEkle(musteri1);
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1;
+            musteri2.TcNo = 1234567892;
+            musteri2.Adi = "veli";
+            musteri2.Soyadi = "yılmaz";
+            musteri2.Il = "Mersin";
+            musteriManager.MusteriEkle(musteri2);
+
+
+            musteriManager.MusteriListele(musteri1,musteri2);
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriSil(musteri2);
         }
     }
 }
